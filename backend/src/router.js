ce883d8ab2dc86e7a6803326652b2630528a8300
Adnan <adbak.dev@gmail.com>
2023-01-02 10:34:52 +0100

    finished quest

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,13 @@
 const express = require("express");
 
+const fs = require("fs");
+
+const multer = require("multer");
+
+const { v4: uuidv4 } = require("uuid");
+
+const upload = multer({ dest: "uploads/" });
+
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
@@ -9,5 +17,18 @@ router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
+
+router.post("/api/avatar", upload.single("avatar"), (req, res) => {
+  const { originalname } = req.file;
+  const { filename } = req.file;
+  fs.rename(
+    `uploads/${filename}`,
+    `uploads/${uuidv4()}-${originalname}`,
+    (err) => {
+      if (err) throw err;
+      res.send("File uploaded");
+    }
+  );
+});
 
 module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
-import Home from "./pages/Home";
+import { useRef } from "react";
+import axios from "axios";
 
 import "./App.css";
 
+const url = import.meta.env.VITE_BACKEND_URL;
+
 function App() {
+  const inputRef = useRef();
+
+  const hSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("avatar", inputRef.current.files[0]);
+    axios.post(url, formData);
+  };
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <form encType="multipart/form-data" onSubmit={hSubmit}>
+        <input type="file" name="avatar" ref={inputRef} />
+        <button type="submit">Send!</button>
+      </form>
     </div>
   );
 }
